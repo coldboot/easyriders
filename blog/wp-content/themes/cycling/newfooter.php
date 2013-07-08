5c42c4dc657e6be26c696de1c5925d4ed004c0de
Nick McClellan <nicholas.mcclellan@gmail.com>
2013-07-09 08:39:36 +1000

    Remove worpdress from source control.
    Will manage wordpress seperately to the php site.

--- blog/wp-content/themes/cycling/newfooter.php
+++
@@ -1,9 +0,0 @@
-<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
-
-      <tr>
-
-        <td height="90" align="center" class="white_text">Copyrights &copy; 2011-2013<a href="/index.php" style="text-decoration:none; color:#666;"> sydney-easy-riders.com.au</a> All Rights Reserved. </td>
-
-        </tr>
-
-    </table>
